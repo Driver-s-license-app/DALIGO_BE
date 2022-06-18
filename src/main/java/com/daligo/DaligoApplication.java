59be45c7d02a376842c7f043564623e35f5b807a
minho <dnjsalsgh123@gmail.com>
2022-06-18 18:30:44 +0900

    spring boot 환경설정

---
+++ src/main/java/com/daligo/DaligoApplication.java
@@ -0,0 +1,13 @@
+package com.daligo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaligoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaligoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daligo/DaligoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daligo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaligoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
